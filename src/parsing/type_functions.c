870be7b1cc29ec6b4e7ea911feafc36fe41ea182
Action2thefuture <action2thefuture@gmail.com>
2024-06-01 11:25:56 +0200

    Modify : Add utils folder in src

--- src/parsing/type_functions.c
+++ src/parsing/type_functions.c
@@ -6,7 +6,7 @@
 /*   By: junsan <junsan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 10:29:28 by junsan            #+#    #+#             */
-/*   Updated: 2024/06/01 11:13:22 by junsan           ###   ########.fr       */
+/*   Updated: 2024/06/01 11:22:53 by junsan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,27 +37,6 @@ bool	is_subshell_operator(const char *token)
 
 	len = ft_strlen(token);
 	return (token[0] == '(' || token[len - 1] == ')');
-}
-
-static const char	*get_path(const char *full_path)
-{
-	const char	*file_name;
-	char		*path;
-	size_t		path_len;
-
-	file_name = ft_strchr(full_path, '/');
-	if (file_name == NULL)
-		return (ft_strdup("."));
-	path_len = file_name - full_path + 1;
-	path = (char *)malloc(sizeof(char) * (path_len + 1));
-	if (path == NULL)
-	{
-		perror("malloc");
-		return (NULL);
-	}
-	ft_strlcpy(path, full_path, path_len + 1);
-	path[path_len] = '\0';
-	return ((const char *)path);
 }
 
 bool	is_file_name(const char *token)
